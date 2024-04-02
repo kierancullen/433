3b3adba02206e528d89e6094312a1d43b7200c72
kierancullen <41768035+kierancullen@users.noreply.github.com>
2024-04-02 14:42:49 -0500

    test reading button in loop

--- pico/homework3/homework3.c
+++ pico/homework3/homework3.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/adc.h"
+#define BOUNCE_DELAY_MS 100
 
 int main() {
     stdio_init_all();
@@ -12,13 +13,34 @@ int main() {
     adc_init();
     adc_gpio_init(26); //use ADC on ADC0 pin
     adc_select_input(0);
- 
+
+    gpio_init(15);
+    gpio_set_dir(15, GPIO_IN);
+    gpio_set_pulls(15, 1, 0);
+   
+    gpio_init(25);
+    gpio_set_dir(25, GPIO_OUT);
+    gpio_put(25, 0);
+    
+    //absolute_time_t lastPressTime = get_absolute_time();
+    int lastButtonValue = gpio_get(15);
+    char message[100];
     while (1) {
-        char message[100];
-        scanf("%s", message);
-        printf("message: %s\r\n",message);
-        sleep_ms(50);
-        uint16_t result = adc_read();
-        printf("adc value: %d\r\n", &result);
+        if (gpio_get(15) == 0) { //beginning edge of button press
+            //uint16_t result = adc_read();
+            //printf("ADC: %d\r\n", result);
+            gpio_put(25, 1);
+            //sleep_ms(50);
+            // if (absolute_time_diff_us(lastPressTime, get_absolute_time()) > 0) { //debouncing delay
+            //     lastPressTime = get_absolute_time();
+            //     uint16_t result = adc_read();
+            //     printf("ADC: %d\r\n", result);
+            // }
+        }
+        else {
+            gpio_put(25, 0);  
+        }  
+        //lastButtonValue = gpio_get(15);
+        
     }
 }
