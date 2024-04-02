3116b62f40197a5266e77fbdeb22803e9e2318b5
kierancullen <41768035+kierancullen@users.noreply.github.com>
2024-04-02 15:12:06 -0500

    complete homework3

--- pico/homework3/homework3.c
+++ pico/homework3/homework3.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/adc.h"
-#define BOUNCE_DELAY_MS 100
+#define ADC_DELAY_MS 10 //for 100Hz read frequency
+#define ADC_SCALE 3.3/4096
 
 int main() {
     stdio_init_all();
@@ -10,37 +11,48 @@ int main() {
     }
     printf("Started.\n");
 
+    gpio_init(25);
+    gpio_set_dir(25, GPIO_OUT);
+    gpio_put(25, 0);
+
     adc_init();
     adc_gpio_init(26); //use ADC on ADC0 pin
     adc_select_input(0);
 
+    gpio_init(0);
+    gpio_set_dir(0, GPIO_IN);
+    gpio_set_pulls(0, 1, 0);
+
     gpio_init(15);
-    gpio_set_dir(15, GPIO_IN);
-    gpio_set_pulls(15, 1, 0);
-   
-    gpio_init(25);
-    gpio_set_dir(25, GPIO_OUT);
-    gpio_put(25, 0);
-    
-    //absolute_time_t lastPressTime = get_absolute_time();
-    int lastButtonValue = gpio_get(15);
-    char message[100];
+    gpio_set_dir(15, GPIO_OUT);
     while (1) {
-        if (gpio_get(15) == 0) { //beginning edge of button press
-            //uint16_t result = adc_read();
-            //printf("ADC: %d\r\n", result);
-            gpio_put(25, 1);
-            //sleep_ms(50);
-            // if (absolute_time_diff_us(lastPressTime, get_absolute_time()) > 0) { //debouncing delay
-            //     lastPressTime = get_absolute_time();
-            //     uint16_t result = adc_read();
-            //     printf("ADC: %d\r\n", result);
-            // }
+        gpio_put(15, 1); //LED on
+    
+        while (gpio_get(0) != 0) { //wait for button to be low (pressed)
+            ;
         }
-        else {
-            gpio_put(25, 0);  
-        }  
-        //lastButtonValue = gpio_get(15);
+
+        gpio_put(15, 0); //LED off
+
+        //Read the number of samples to take
+        int sampleCount;
+        printf("Enter a number of analog voltage samples to take, between 1 and 100: ");
+        scanf("%d", &sampleCount);
+        printf("\r\n");
         
-    }
+        //Collect the samples
+        uint16_t resultsRaw[sampleCount];
+        int i = 0;
+        while (i < sampleCount) {
+            resultsRaw[i] = adc_read();
+            i++;
+            sleep_ms(ADC_DELAY_MS);
+        }
+
+        //Print the samples back to the terminal
+        for (i = 0; i < sampleCount; i++) {
+            printf("Sample %d: %.3f V\r\n", i+1, resultsRaw[i]*ADC_SCALE);
+        }
+        
+    } 
 }
