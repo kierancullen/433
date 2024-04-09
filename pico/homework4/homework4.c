4fc7d450d86a25c9cf6d07bb2b5bb356a397bfff
kierancullen <41768035+kierancullen@users.noreply.github.com>
2024-04-09 17:40:26 -0500

    write the SPI bits in the correct order

--- pico/homework4/homework4.c
+++ pico/homework4/homework4.c
@@ -16,17 +16,18 @@ static inline void csDeselect() {
     asm volatile("nop \n nop \n nop");
 }
 
-static void writeRegister(bool useDacA, uint16_t dacInput16, uint8_t * src) {
-    src[0] = (uint8_t)((dacInput16 << 2) & 0xFF); //fill the lowest two bits with zeros, then take the lowest eight bits
-    src[1] = (uint8_t)((dacInput16 >> 6) & 0x0F); //fill the lowest two bits with zeros,, then take only the top four bits
+static void writeRegister(bool useDacA, uint16_t dacInput16) {
+    uint8_t src[2];
+    src[1] = (uint8_t)((dacInput16 << 2) & 0xFF); //fill the lowest two bits with zeros, then take the lowest eight bits
+    src[0] = (uint8_t)((dacInput16 >> 6) & 0x0F); //fill the lowest two bits with zeros,, then take only the top four bits
 
-    src[1] = src[1] | (!useDacA << 7); //set bit 15 to 1 if not using DacA, 0 otherwise
-    src[1] = src[1] | (0b111 << 4); //always use buffering, gain 1, not shutdown
+    src[0] = src[0] | (!useDacA << 7); //set bit 15 to 1 if not using DacA, 0 otherwise
+    src[0] = src[0] | (0b111 << 4); //always use buffering, gain 1, not shutdown
     
-    // csSelect();
-    // spi_write_blocking(spi_default, &dacInput, 2);
-    // csDeselect();
-    // sleep_ms(10);
+    csSelect();
+    spi_write_blocking(spi_default, src, 2);
+    csDeselect();
+    sleep_ms(10);
 }
 
 static uint16_t getDacInput16 (float refRatio) { //refRatio is <desired vOut> / <reference voltage>
@@ -54,11 +55,10 @@ int main() {
         scanf("%f", &voltage);
         printf("\r\n");
 
-        uint8_t src[2];
         uint16_t input = getDacInput16(voltage/3.3);
-        writeRegister(0, input, src);
-        printf("The 16-bit DAC input is %x \r\n", input);
-        printf("The SPI output is %x, %x \r\n", src[0], src[1]);
+        printf("The 10-bit input is 0x%x\r\n", input);
+        writeRegister(0, input);
+
     }
     
 }
