5f0de86e84358ca62c01fc12c284742eb044d18c
kierancullen <41768035+kierancullen@users.noreply.github.com>
2024-04-16 19:13:30 -0500

    use internal pull-up resistors on SDA and SCL for homework5

--- pico/homework5/homework5.c
+++ pico/homework5/homework5.c
@@ -36,8 +36,6 @@ uint8_t readPins() {
     return buf[0];
 }
 
-//some sort of write function
-
 int main() {
     stdio_init_all();
 
@@ -48,8 +46,8 @@ int main() {
     i2c_init(i2c_default, 100 * 1000);
     gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
     gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
-    //gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
-    //gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN); //we have physical have pull-up resistors on the board 
+    gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
+    gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN); //use these instead of pull-up resistors on the board 
 
     //initialize the MCP23008
     init();
